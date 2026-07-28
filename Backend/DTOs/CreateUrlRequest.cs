090250dae4d82ce84ddd65a548cd0b91d157584b
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-07-28 17:01:21 -0400

    added posting longurl to the database

--- Backend/DTOs/CreateUrlRequest.cs
+++ Backend/DTOs/CreateUrlRequest.cs
@@ -3,5 +3,5 @@ using System.Text.Json.Serialization;
 public class CreateUrlRequest
 {
     [JsonPropertyName("long_url")]
-    public required string LongUrl { get; set; }
+    public string LongUrl { get; set; } = "";
 }
