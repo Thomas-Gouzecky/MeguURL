4325606722492d3011d7817b4ea9c0d38c1a56ce
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-07-29 18:35:06 -0400

    added redirection for some errors

--- Backend/Controllers/RedirectController.cs
+++ Backend/Controllers/RedirectController.cs
@@ -1,3 +1,4 @@
+using backend.Components.Pages;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers;
@@ -16,6 +17,10 @@ public class RedirectController : ControllerBase
     public async Task<IActionResult> GetRedirectURL(string code)
     {
         var response = await _http.GetAsync($"api/urls/{code}");
+
+        // Redirect to a 404 not found page?
+        if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+            return Redirect("/error");
 
         if (!response.IsSuccessStatusCode)
         {
