bb48c6a267204ec84d81ddbe5898d98beeb837f5
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-07-30 14:57:49 -0400

    added validation logic and route

--- Backend/Services/ValidationService/ValidateUrl/UrlValidator.cs
+++ Backend/Services/ValidationService/ValidateUrl/UrlValidator.cs
@@ -1,4 +1,14 @@
 public class UrlValidator : IValidateBase
 {
-    public bool Validate(object url) { return true; }
+    public bool Validate(object context)
+    {
+
+        if (context is not string url || string.IsNullOrWhiteSpace(url))
+        {
+            return false;
+        }
+
+        return Uri.TryCreate(url, UriKind.Absolute, out var uri) &&
+                (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps);
+    }
 }
