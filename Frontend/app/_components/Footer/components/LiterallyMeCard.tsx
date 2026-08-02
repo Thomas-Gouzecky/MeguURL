ce9607ef9ec428b3c1c109ea7ece2e3d97fb9491
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-08-02 12:22:22 -0400

    changed image card to use next image component

--- Frontend/app/_components/Footer/components/LiterallyMeCard.tsx
+++ Frontend/app/_components/Footer/components/LiterallyMeCard.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
+
 export default function LiterallyMe() {
 	return (
 		<div className="max-w-60 p-3 rounded-xl bg-[#211615] border-3 border-[#332320] shadow-md">
 			<p>Literally me :3</p>
-			<img
+			<Image
 				src="/literally-me.jpg"
 				className="rounded-lg"
+				alt="Literally me as a sleepy cat ^_^"
+				width={512}
+				height={512}
 			/>
 		</div>
 	);
