eda88de41f89ea4a53cac3595361f1c02a4d8198
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-08-03 15:16:51 -0400

    lg

--- Frontend/app/_components/Footer/components/FooterCard.tsx
+++ Frontend/app/_components/Footer/components/FooterCard.tsx
@@ -15,7 +15,7 @@ export default function FootCard({
 		<div className={containerCSS}>
 			{Title ? (
 				<div className="w-full flex flex-col items-center">
-					<h1 className="text-md font-bold custom-text-primary">{Title}</h1>
+					<h1 className="text-lg font-bold custom-text-primary">{Title}</h1>
 					<div className="h-0.5 w-2/3 my-2 bg-linear-to-r from-transparent via-[#2f251c] to-transparent rounded-full transform group-hover:w-full transition-all duration-500 animate-pulse" />
 				</div>
 			) : null}
