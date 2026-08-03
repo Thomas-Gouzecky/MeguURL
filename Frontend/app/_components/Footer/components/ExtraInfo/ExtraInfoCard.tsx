e2d542c6fecdc0c10227dcdbc688ef49f6c92b2e
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-08-03 16:20:32 -0400

    fixed extra info link underline effect

--- Frontend/app/_components/Footer/components/ExtraInfo/ExtraInfoCard.tsx
+++ Frontend/app/_components/Footer/components/ExtraInfo/ExtraInfoCard.tsx
@@ -8,7 +8,7 @@ export default function ExtraInfoCard({ ExtraInfo }: { ExtraInfo: ExtraInfoProp 
 		>
 			{ExtraInfo.name}
 
-			<span className="absolute bottom-0 left-1/2 h-0.5 w-2/3 -translate-x-1/2 scale-x-0 bg-gradient-to-r from-transparent via-[#FADA64] to-transparent transition-transform duration-500 group-hover:scale-x-100" />
+			<span className="absolute bottom-0 left-1/2 h-0.5 w-0 -translate-x-1/2 bg-linear-to-r from-transparent via-[#FADA64] to-transparent transition-all duration-500 group-hover:w-3/4" />
 		</Link>
 	);
 }
