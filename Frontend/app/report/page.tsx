2861752ed973ff59d2cace186f835c9e72fb166a
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-08-05 21:15:28 -0400

    added starter pages to other routes

---
+++ Frontend/app/report/page.tsx
@@ -0,0 +1,7 @@
+export default function Report() {
+	return (
+		<div>
+			<p>This is a report section!</p>
+		</div>
+	);
+}
---
+++ Frontend/app/terms-of-service/page.tsx
@@ -0,0 +1,7 @@
+export default function TermsOfService() {
+	return (
+		<div>
+			<p>This is a Terms of Service section!</p>
+		</div>
+	);
+}
