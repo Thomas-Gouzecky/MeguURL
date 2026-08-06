b9f52dddc523fb4c4a5c55ba0bcd8dc05e78ba83
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-08-05 21:29:36 -0400

    added cursor gifs

--- Frontend/app/_components/Body/Home/InputSection/InputSection.tsx
+++ Frontend/app/_components/Body/Home/InputSection/InputSection.tsx
@@ -2,7 +2,7 @@ import InputForm from "./InputForm";
 
 export default function InputSection() {
 	return (
-		<div className="bg-blue-950 p-4">
+		<div className="bg-blue-950 p-4 custom-text-primary">
 			<span>Paste your url to explode it!</span>
 			<InputForm />
 		</div>
