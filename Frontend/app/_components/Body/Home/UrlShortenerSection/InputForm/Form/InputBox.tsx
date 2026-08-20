553cb906e27af629a8a84e4786b7c26bc135d547
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-08-20 17:04:45 -0400

    keeps the value inputted by the user (i like this because it doesnt remove the url after submission)

--- Frontend/app/_components/Body/Home/UrlShortenerSection/InputForm/Form/InputBox.tsx
+++ Frontend/app/_components/Body/Home/UrlShortenerSection/InputForm/Form/InputBox.tsx
@@ -1,6 +1,8 @@
 import { useFormStatus } from "react-dom";
+import { useState } from "react";
 
 export default function InputBox({ status }: { status: StatusProp }) {
+	const [inputUrl, setInputUrl] = useState<string>("");
 	const { pending } = useFormStatus();
 
 	const statusStyle: Record<Status, string> = {
@@ -14,10 +16,12 @@ export default function InputBox({ status }: { status: StatusProp }) {
 
 	return (
 		<input
-			className={`${statusStyle[status.state]} disabled:bg-amber-50 flex-1 z-10 origin-center bg-[#130b0b] outline-none focus:scale-100 hover:shadow-[0_0_10px_3px_rgba(0,0,0,0.25)] border-2 button-padding button-rounding relative text-lg font-bold overflow-hidden transition-all duration-300`}
+			className={`${statusStyle[status.state]} flex-1 z-10 origin-center bg-[#130b0b] outline-none focus:scale-100 hover:shadow-[0_0_10px_3px_rgba(0,0,0,0.25)] border-2 button-padding button-rounding relative text-lg font-bold overflow-hidden transition-all duration-300`}
 			name="inputUrl"
+			value={inputUrl}
 			type="text"
 			placeholder="Example: google.com"
+			onChange={(e) => setInputUrl(e.target.value)}
 			disabled={pending}
 		/>
 	);
