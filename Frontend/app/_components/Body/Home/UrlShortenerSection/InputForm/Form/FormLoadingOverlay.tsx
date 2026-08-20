40f41adf4ca4c6dbbe5322e567f86dd8b6371b2b
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-08-20 18:42:26 -0400

    added some animations to the loading overlay (i still want to add a better loading icon but this is fine for now)

--- Frontend/app/_components/Body/Home/UrlShortenerSection/InputForm/Form/FormLoadingOverlay.tsx
+++ Frontend/app/_components/Body/Home/UrlShortenerSection/InputForm/Form/FormLoadingOverlay.tsx
@@ -1,14 +1,78 @@
-import { motion } from "motion/react";
+import { motion, Variants } from "motion/react";
 import { useFormStatus } from "react-dom";
+import { useEffect, useState } from "react";
 
 export default function FormLoadingOverlay() {
 	const { pending } = useFormStatus();
+	const [animationState, setAnimationState] = useState<"visible" | "hidden">("hidden");
 
-	if (!pending) return null;
+	const variant: Variants = {
+		hidden: {
+			opacity: 1,
+			transition: {
+				staggerChildren: 0.2,
+				staggerDirection: -1,
+			},
+		},
+		visible: {
+			opacity: 1,
+			transition: {
+				staggerChildren: 0.2,
+			},
+		},
+	};
+
+	const item: Variants = {
+		hidden: {
+			opacity: 0,
+			scale: 0.95,
+		},
+		visible: {
+			opacity: 1,
+			scale: 1,
+			transition: {
+				type: "spring",
+			},
+		},
+	};
+
+	const background: Variants = {
+		hidden: {
+			opacity: 0,
+		},
+		visible: {
+			opacity: 1,
+			transition: {
+				ease: "easeOut",
+			},
+		},
+	};
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setAnimationState(pending ? "visible" : "hidden");
+		}, 100);
+
+		return () => clearTimeout(timeout);
+	}, [pending]);
 
 	return (
-		<div className="absolute inset-0 z-50 flex items-center justify-center rounded-lg bg-black/40">
-			<div className="animate-spin">{/* spinner */} Loading...</div>
-		</div>
+		<motion.div
+			className={`flex items-center justify-center absolute inset-0 z-50 rounded-lg ${pending ? "pointer-events-auto" : "pointer-events-none"}`}
+			variants={variant}
+			initial="hidden"
+			animate={animationState}
+		>
+			<motion.div
+				className={`absolute z-5 w-full h-full rounded-lg inset-0 bg-black/40`}
+				variants={background}
+			/>
+			<motion.div
+				className="absolute z-10 custom-text-primary font-bold"
+				variants={item}
+			>
+				{/* spinner */} Loading...
+			</motion.div>
+		</motion.div>
 	);
 }
