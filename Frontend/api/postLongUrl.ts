f9eb69081f676fd07c8c3da63840aab0804b4659
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-08-20 19:46:46 -0400

    testing (log frontend stuff)

--- Frontend/api/postLongUrl.ts
+++ Frontend/api/postLongUrl.ts
@@ -5,6 +5,8 @@ type ApiPostUrlResponse = {
 
 export default async function postLongUrl(longUrl: string): Promise<PostUrlResponse> {
 	const endpoint = process.env.DEPLOYMENT === "k8s" ? "" : (process.env.NEXT_PUBLIC_BACKEND ?? "");
+	console.log("Backend: ", process.env.NEXT_PUBLIC_BACKEND);
+	console.log("Deployment: ", process.env.DEPLOYMENT);
 	try {
 		if (longUrl.trim() === "") {
 			return {
@@ -16,7 +18,7 @@ export default async function postLongUrl(longUrl: string): Promise<PostUrlRespo
 				},
 			};
 		}
-		
+
 		const response = await fetch(`${endpoint}/api/urls/`, {
 			method: "POST",
 			headers: {
