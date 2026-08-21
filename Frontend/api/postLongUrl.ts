b522b2fb571d73397d0a8712f483beccdb397dd8
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-08-20 21:33:35 -0400

    updated frontend to use better routing for multiple deployments

--- Frontend/api/postLongUrl.ts
+++ Frontend/api/postLongUrl.ts
@@ -4,22 +4,19 @@ type ApiPostUrlResponse = {
 };
 
 export default async function postLongUrl(longUrl: string): Promise<PostUrlResponse> {
-	const endpoint = process.env.DEPLOYMENT === "k8s" ? "" : (process.env.NEXT_PUBLIC_BACKEND ?? "");
-	console.log("Backend: ", process.env.NEXT_PUBLIC_BACKEND);
-	console.log("Deployment: ", process.env.DEPLOYMENT);
-	try {
-		if (longUrl.trim() === "") {
-			return {
-				success: false,
-				error: {
-					name: "Invalid URL",
-					code: 400,
-					description: { error: "Please enter a URL." },
-				},
-			};
-		}
+	if (longUrl.trim() === "") {
+		return {
+			success: false,
+			error: {
+				name: "Invalid URL",
+				code: 400,
+				description: { error: "Please enter a URL." },
+			},
+		};
+	}
 
-		const response = await fetch(`${endpoint}/api/urls/`, {
+	try {
+		const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND}/api/urls/`, {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
