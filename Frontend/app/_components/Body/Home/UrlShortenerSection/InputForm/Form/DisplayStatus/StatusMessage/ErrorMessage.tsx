c9429ff84475fdbd1421fbcfb8d69c4fe2597cad
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-08-21 21:13:01 -0400

    updated error message to allow users to select the url they inputted

--- Frontend/app/_components/Body/Home/UrlShortenerSection/InputForm/Form/DisplayStatus/StatusMessage/ErrorMessage.tsx
+++ Frontend/app/_components/Body/Home/UrlShortenerSection/InputForm/Form/DisplayStatus/StatusMessage/ErrorMessage.tsx
@@ -3,21 +3,23 @@ import Link from "next/link";
 export default function ErrorMessage({ Error }: { Error: PostUrlErrorResponse }) {
 	return (
 		<div className="flex flex-col min-w-0">
-			<div className="flex min-w-0">
-				<span className="shrink-0">
-					{Error.status} - {Error.title}
-				</span>
+			<div className="flex min-w-0 w-full">
+				<div className="shrink-0">
+					<span>{Error.status}</span>
+					<span>&nbsp;-&nbsp;{Error.title}</span>
+				</div>
 
 				{Error.url && (
-					<span className="min-w-0 overflow-hidden text-ellipsis whitespace-nowrap">
-						:&nbsp;
+					<div className="flex min-w-0 shrink">
+						<span>:&nbsp;</span>
 						<Link
+							className="min-w-0 overflow-hidden text-ellipsis whitespace-nowrap clickable-text-color transition-all duration-500"
 							href={Error.url!}
 							target="_blank"
 						>
 							{Error.url}
 						</Link>
-					</span>
+					</div>
 				)}
 			</div>
 
@@ -28,7 +30,7 @@ export default function ErrorMessage({ Error }: { Error: PostUrlErrorResponse })
 						<span className="shrink-0">'</span>
 
 						<Link
-							className="min-w-0 overflow-hidden text-ellipsis whitespace-nowrap"
+							className="min-w-0 overflow-hidden text-ellipsis whitespace-nowrap clickable-text-color transition-all duration-500"
 							href={Error.url!}
 							target="_blank"
 						>
