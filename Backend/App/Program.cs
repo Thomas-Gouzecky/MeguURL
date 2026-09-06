854cf7718064215396e5d2d72daf9f0b9a636ea2
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-09-06 12:24:10 -0400

    added http client for app

--- Backend/App/Program.cs
+++ Backend/App/Program.cs
@@ -36,10 +36,14 @@ var backendApi = builder.Configuration["ApiSettings:BackendApi"]
 var databaseApi = builder.Configuration["ApiSettings:DatabaseApi"]
     ?? throw new InvalidOperationException("Database API URL is missing");
 
+var qrcodeApi = builder.Configuration["ApiSettings:QrCodeApi"]
+    ?? throw new InvalidOperationException("QR Code API URL is missing");
+
 builder.Services.AddTransient<ApiExceptionHandler>();
 
 builder.Services.AddHttpClient("BackendApi", client => { client.BaseAddress = new Uri(backendApi); });
 builder.Services.AddHttpClient("DatabaseApi", client => { client.BaseAddress = new Uri(databaseApi); }).AddHttpMessageHandler<ApiExceptionHandler>();;
+builder.Services.AddHttpClient("QrCodeApi", client => { client.BaseAddress = new Uri(qrcodeApi); }).AddHttpMessageHandler<ApiExceptionHandler>();;
 builder.Services.AddHttpClient("UrlValidator", client => { client.Timeout = TimeSpan.FromSeconds(10); });
     
 builder.Services.AddSingleton<UrlCodeService>();
