4c88f212e42904341c1f3a82233750551ef41db3
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-09-09 19:48:57 -0400

    updated the look of the nav items. will probably change them again

--- Frontend/app/_components/Navbar/components/NavItem.tsx
+++ Frontend/app/_components/Navbar/components/NavItem.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion, Variants } from "motion/react";
 import Link from "next/link";
 
 import { usePathname } from "next/navigation";
@@ -8,19 +9,60 @@ export default function NavItem({ NavItem }: { NavItem: NavItemProp }) {
 	const pathname = usePathname();
 	const active = pathname === NavItem.href;
 
+	const checkActive: Variants = {
+		hidden: {
+			opacity: 0,
+			scale: 0.95,
+		},
+		visible: {
+			opacity: active ? 0.5 : 1,
+			scale: 1,
+		},
+	};
+
+	const defaultItem: Variants = {
+		hidden: {
+			opacity: 0,
+			scale: 0.95,
+		},
+		visible: {
+			opacity: 1,
+			scale: 1,
+			backgroundColor: active ? "rgba(0, 0, 0, 0)" : "#981f1f",
+			transition: {
+				type: "spring",
+			},
+		},
+		hover: {
+			scale: 1.05,
+			backgroundColor: "#981f1f",
+		},
+		tap: {
+			scale: 0.95,
+			backgroundColor: "#7a1a1a",
+		},
+	};
+
 	return (
-		<Link
-			href={NavItem.href}
-			aria-disabled={active}
-			tabIndex={active ? -1 : undefined}
-			className={
-				active
-					? "pointer-events-none opacity-50"
-					: "" +
-						`border-transparent border-3 button-padding button-rounding text-2xl text-shadow-md font-bold transition-all duration-300 hover:bg-[#260707] hover:inset-shadow-2xs hover:border-[#5E3131] hover:-translate-y-1.5 hover:shadow-[0px_6px_0px_rgba(0,0,0,0.2)] active:translate-y-1 active:shadow-none`
-			}
+		<motion.div
+			variants={checkActive}
+			className="relative"
 		>
-			{NavItem.name}
-		</Link>
+			<Link
+				href={NavItem.href}
+				aria-disabled={active}
+				className={`button-rounding ${active ? "pointer-events-none" : ""}`}
+			>
+				<motion.div
+					className={`relative button-padding button-rounding text-2xl text-shadow-md font-bold`}
+					variants={defaultItem}
+					layout
+					whileHover={active ? undefined : "hover"}
+					whileTap="tap"
+				>
+					{NavItem.name}
+				</motion.div>
+			</Link>
+		</motion.div>
 	);
 }
