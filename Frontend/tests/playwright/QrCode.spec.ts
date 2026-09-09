05724c6b42c9e9817c00918a1b5ce29b6a1fab53
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-09-09 14:38:28 -0400

    playwright test to check the home page for now

---
+++ Frontend/tests/playwright/QrCode.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("QR Code Creation Page", () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto("");
+	});
+
+	test("Check Home Page is Reachable", async ({ page }) => {
+		await expect(page).toHaveURL("http://localhost:3000");
+	});
+});
