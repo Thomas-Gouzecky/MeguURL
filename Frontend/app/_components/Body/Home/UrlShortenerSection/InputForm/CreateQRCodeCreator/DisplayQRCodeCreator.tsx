4d130e45d6c0e3119481df8644d0467de0951c9b
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-09-10 12:53:50 -0400

    added the icon and made it animated

--- Frontend/app/_components/Body/Home/UrlShortenerSection/InputForm/CreateQRCodeCreator/DisplayQRCodeCreator.tsx
+++ Frontend/app/_components/Body/Home/UrlShortenerSection/InputForm/CreateQRCodeCreator/DisplayQRCodeCreator.tsx
@@ -1,6 +1,9 @@
-import { useSyncExternalStore } from "react";
+import { useState, useSyncExternalStore } from "react";
+import DisplayQRCodeIcon from "./QRCodeIcon";
+import { motion } from "motion/react";
 
 export default function DisplayQRCodeCreator({ code }: { code: string | undefined }) {
+	const [hover, setHover] = useState(false);
 	const baseURL = useSyncExternalStore(
 		() => () => {},
 		() => window.location.host,
@@ -10,9 +13,20 @@ export default function DisplayQRCodeCreator({ code }: { code: string | undefine
 	return (
 		<div>
 			{code && (
-				<div className="h-15 w-full flex justify-center items-center">
-					Hello {baseURL}/{code}
-				</div>
+				<motion.div
+					className="h-15 w-full flex justify-center items-center"
+					onHoverStart={() => {
+						setHover(true);
+					}}
+					onHoverEnd={() => {
+						setHover(false);
+					}}
+				>
+					<div className="clickable-text-color bg-[#471414] flex border-transparent border-2 button-rounding transition-all duration-300 hover:bg-[#260707] hover:inset-shadow-2xs hover:border-[#5E3131]">
+						<DisplayQRCodeIcon hover={hover} />
+					</div>
+					Generate into QR Code
+				</motion.div>
 			)}
 		</div>
 	);
