793fcc2f5abcbedcbf53a61e486e9f0a1c895699
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-09-11 11:32:53 -0400

    added a rounding to the border

--- Frontend/app/qrcode/QRCodeImage/QRCodeImage.tsx
+++ Frontend/app/qrcode/QRCodeImage/QRCodeImage.tsx
@@ -1,7 +1,7 @@
 export default function QRCodeImage({ QRCode }: { QRCode: React.ReactNode }): React.JSX.Element {
 	return (
-		<div className="shrink-0 w-[210px] overflow-visible">
-			<div>{QRCode}</div>
+		<div className="shrink-0 overflow-visible">
+			<div className="overflow-hidden rounded-lg">{QRCode}</div>
 		</div>
 	);
 }
