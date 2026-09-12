dc9c6edec90e6c580512a7212b6ffb4b3b446b53
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-09-12 12:14:01 -0400

    added env for reaching the backend. added more test browsers

--- Frontend/playwright.config.ts
+++ Frontend/playwright.config.ts
@@ -49,43 +49,47 @@ export default defineConfig({
 			use: { ...devices["Desktop Chrome"] },
 		},
 
-		// {
-		// 	name: "firefox",
-		// 	dependencies: ["setup"],
-		// 	use: { ...devices["Desktop Firefox"] },
-		// },
+		{
+			name: "firefox",
+			dependencies: ["setup"],
+			use: { ...devices["Desktop Firefox"] },
+		},
 
-		// {
-		// 	name: "webkit",
-		// 	dependencies: ["setup"],
-		// 	use: { ...devices["Desktop Safari"] },
-		// },
+		{
+			name: "webkit",
+			dependencies: ["setup"],
+			use: { ...devices["Desktop Safari"] },
+		},
 
 		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: { ...devices['Pixel 5'] },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: { ...devices['iPhone 12'] },
-		// },
+		{
+			name: "Mobile Chrome",
+			use: { ...devices["Pixel 5"] },
+		},
+		{
+			name: "Mobile Safari",
+			use: { ...devices["iPhone 12"] },
+		},
 
 		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-		// },
+		{
+			name: "Microsoft Edge",
+			use: { ...devices["Desktop Edge"], channel: "msedge" },
+		},
+		{
+			name: "Google Chrome",
+			use: { ...devices["Desktop Chrome"], channel: "chrome" },
+		},
 	],
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: "npm run start",
+		command: "npm run build && npm run start",
 		url: baseURL,
+		env: {
+			...process.env,
+			NEXT_PUBLIC_BACKEND: process.env.NEXT_PUBLIC_BACKEND ?? "http://127.0.0.1:6767",
+		},
 		reuseExistingServer: !process.env.CI,
 	},
 });
